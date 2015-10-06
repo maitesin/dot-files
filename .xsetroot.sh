fe008c6f4b4290d71c2171fe2fa03c7786deff66
Maitesin <violador.de.segmentos@gmail.com>
2015-10-06 10:39:21 +0100

    dot files for my DWM configuration

---
+++ .xsetroot.sh
@@ -0,0 +1,7 @@
+DATETIME=`date`
+BATTERYSTATE=$( acpi -b | awk '{ split($5,a,":"); print substr($3,0,2), $4, "["a[1]":"a[2]"]" }' | tr -d ',' )
+HOME_HDD=$( df -h | grep home | awk '{print $4}' )
+SYS_HDD=$( df -h | grep /dev/sda2 | awk '{print $4}' )
+CPU=$( mpstat | grep all | awk '{print $3}' )
+MEM=$( free -m | grep Mem | awk '{print $7}' )
+xsetroot -name "CPU: ${CPU} | MEM: ${MEM}MB | System: ${SYS_HDD} | Home: ${HOME_HDD} | ${DATETIME} | Battery: ${BATTERYSTATE}"
