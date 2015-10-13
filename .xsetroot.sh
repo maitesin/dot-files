eeab3331cfb2f1e0c2790c8d3254e196b843812f
Oscar Forner Martinez <oscar_martinez@programmingresearch.com>
2015-10-13 09:51:35 +0100

    Changes in some configuration files.

--- .xsetroot.sh
+++ .xsetroot.sh
@@ -4,4 +4,4 @@ HOME_HDD=$( df -h | grep home | awk '{print $4}' )
 SYS_HDD=$( df -h | grep /dev/sda2 | awk '{print $4}' )
 CPU=$( mpstat | grep all | awk '{print $3}' )
 MEM=$( free -m | grep Mem | awk '{print $7}' )
-xsetroot -name "CPU: ${CPU} | MEM: ${MEM}MB | System: ${SYS_HDD} | Home: ${HOME_HDD} | ${DATETIME} | Battery: ${BATTERYSTATE}"
+xsetroot -cursor_name left_ptr -name "CPU: ${CPU} | MEM: ${MEM}MB | System: ${SYS_HDD} | Home: ${HOME_HDD} | ${DATETIME} | Battery: ${BATTERYSTATE}"
