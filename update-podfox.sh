b2e7b90ccf60f8d54664f12603b224bddf9d1cd4
Oscar Forner Martinez <oscar_martinez@programmingresearch.com>
2015-11-13 07:38:49 +0000

    Script to update the podcasts you have configured on podfox

---
+++ update-podfox.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FEEDS=$(podfox feeds | cut -d"|" -f2 | grep -v shortname | grep -v "=" | awk '{print $2}')
+
+for feed in ${FEEDS}
+do
+    echo "Checking ${feed}"
+    podfox update ${feed}
+    podfox episodes ${feed} | grep "Not Downloaded" | head -10
+    read -p "Do you want to download episodes? (Y/n)" download
+    : ${download:="y"}
+    if [[ ${download} == "y" ]]
+    then
+        read -p "How many do you want to download?" howmany
+        : ${howmany:=1}
+        if [[ ${howmany} == 1 ]]
+        then
+            podfox download ${feed} --how-many=${howmany}
+        fi
+    fi
+done    
+
