7d8dd81259b5c13897fb3a695effe25899aa7b02
Maitesin <violador.de.segmentos@gmail.com>
2016-04-22 23:54:43 +0100

    Updated the i3block status

--- i3wm/scripts/volume.sh
+++ i3wm/scripts/volume.sh
@@ -1,11 +1,11 @@
-pavu_status=$(pulseaudio-ctl full-status)
-pavu_status=($pavu_status)
+#!/bin/bash
 
-volume=${pavu_status[0]}
-muted=${pavu_status[1]}
+volume=$(pactl list sinks | grep -B1 '^[[:space:]]Volume:' | tail -n 2 | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+muted=$(pactl list sinks | grep -B1 '^[[:space:]]Volume:' | tail -n 2 | head -n 1 | awk '{print $2}')
 
-if [ "$muted" == "yes" ]; then
-  echo '<span foreground="#Cd3f45">Mute</span>'
+if [[ "$muted" == "yes" ]]
+then
+  echo 'Mute'
 else
   echo $volume
 fi
