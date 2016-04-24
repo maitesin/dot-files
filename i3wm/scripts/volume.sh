274a478ec315e1ba1d4d77619d0d94b48eb70541
Maitesin <violador.de.segmentos@gmail.com>
2016-04-24 12:14:50 +0100

    Some fixes in the volumen script

--- i3wm/scripts/volume.sh
+++ i3wm/scripts/volume.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-volume=$(pactl list sinks | grep -B1 '^[[:space:]]Volume:' | tail -n 2 | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
-muted=$(pactl list sinks | grep -B1 '^[[:space:]]Volume:' | tail -n 2 | head -n 1 | awk '{print $2}')
+volume=$(pactl list sinks | grep -B1 '^[[:space:]]Volume:' | tail -n 5 | head -n 2 | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+muted=$(pactl list sinks | grep -B1 '^[[:space:]]Volume:' | tail -n 5 | head -n 1 | awk '{print $2}')
 
 if [[ "$muted" == "yes" ]]
 then
