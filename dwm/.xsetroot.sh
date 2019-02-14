552ea9f479c7651027bf6a54b6b1ac1e77ef61e7
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2019-02-14 22:23:34 +0000

    Add simple trayer to the system

--- dwm/.xsetroot.sh
+++ dwm/.xsetroot.sh
@@ -2,4 +2,4 @@ DATETIME=`date "+%H:%M %d/%m/%y"`
 BATTERYSTATE=$( acpi -b | awk '{ split($5,a,":"); print substr($3,0,2), $4, "["a[1]":"a[2]"]" }' | tr -d ',' )
 CPU=$( mpstat | grep all | awk '{print $3}' )
 MEM=$( free -m | grep Mem | awk '{print $7}' )
-xsetroot -cursor_name left_ptr -name "CPU: ${CPU} | RAM: ${MEM}MB | Battery: ${BATTERYSTATE} | ${DATETIME} "
+xsetroot -cursor_name left_ptr -name "CPU: ${CPU} | RAM: ${MEM}MB | Battery: ${BATTERYSTATE} | ${DATETIME}           "
