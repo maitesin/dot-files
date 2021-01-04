4ec73cf2f10461e8eb802df770ed2dcf074fcc08
Oscar Forner Martinez <oscar.forner@paack.co>
2021-01-04 20:09:30 +0100

    Update the flags for exa and bat binaries

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,5 @@ set -gx PATH $PATH $HOME/dev/go/bin $HOME/.cargo/bin
 set -gx GOPATH $HOME/dev/go
 set -gx EDITOR nvim
 
-alias ls="exa"
-alias cat="bat"
+alias ls="exa -lgHS --git --time-style long-iso --extended"
+alias cat="bat --theme gruvbox"
