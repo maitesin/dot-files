c8ea1989ac71045c066ea323245a7ca34d6da755
Oscar Forner Martinez <oscar.forner@paack.co>
2021-02-11 20:19:37 +0100

    Add clipit to the setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -62,7 +62,8 @@ sudo apt install -y \
 	buku \
 	liferea \
 	fonts-powerline \
-	fonts-hack-ttf
+	fonts-hack-ttf \
+  clipit
 
 # Download vim-plug to manage NeoVim's plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
