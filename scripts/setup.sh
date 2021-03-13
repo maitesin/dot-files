039a6302db22477c4db0b51ff62add19d56229cf
Oscar Forner Martinez <oscar.forner@paack.co>
2021-03-13 16:38:30 +0100

    Add mdp to the programs to be installed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -63,7 +63,8 @@ sudo apt install -y \
 	liferea \
 	fonts-powerline \
 	fonts-hack-ttf \
-  clipit
+  clipit \
+  mdp
 
 # Download vim-plug to manage NeoVim's plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
