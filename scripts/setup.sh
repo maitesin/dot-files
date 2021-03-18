7b7bbf324ea88cefcf9b27305f86a4b802911e3b
Oscar Forner Martinez <oscar.forner@paack.co>
2021-03-18 10:04:27 +0100

    Add tmux to the list of applications to be installed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -64,7 +64,8 @@ sudo apt install -y \
 	fonts-powerline \
 	fonts-hack-ttf \
   clipit \
-  mdp
+  mdp \
+  tmux
 
 # Download vim-plug to manage NeoVim's plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
