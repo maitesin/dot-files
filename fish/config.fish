e0c5b34cd4e12ebffe72dec6124d98d2f87b133f
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2021-07-22 11:22:34 +0200

    Add asdf check

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,10 @@
 set -gx GOPATH $HOME/dev/go
 set -gx PATH $PATH $GOPATH/bin $HOME/.cargo/bin
 set -gx EDITOR nvim
+set -gx GOPRIVATE github.com/sketch-hq
 
 alias ls="exa -lgHSI snap --git --time-style long-iso --extended"
 alias cat="bat --theme gruvbox"
 alias grep="rg"
+
+source /usr/local/opt/asdf/asdf.fish
