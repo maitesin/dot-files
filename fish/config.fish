1b7ccde5f35b6a943c459c559e35c1ee48f6fd8e
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2021-10-06 09:18:45 +0200

    Add (optionally) the MacOS path to the sketchtool

--- fish/config.fish
+++ fish/config.fish
@@ -11,3 +11,7 @@ alias grep="rg"
 if test -f /usr/local/opt/asdf/asdf.fish
   source /usr/local/opt/asdf/asdf.fish
 end
+
+if test -d /Applications/Sketch.app/Contents/MacOS
+  set -gx PATH $PATH /Applications/Sketch.app/Contents/MacOS
+end
