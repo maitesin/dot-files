244e9c8bf7950aeb896852fdb51361979390fe75
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2021-12-12 11:25:47 +0100

    Add script to compile erlang in M1 Macs

---
+++ scripts/compile_erlang_in_m1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export ARCH="arm64"
+export CC="/usr/bin/gcc"
+export KERL_CONFIGURE_OPTIONS="--with-ssl=/usr/local/opt/openssl@1.1 \
+  --with-wx-config=/usr/local/opt/wxwidgets/bin/wx-config \
+  --without-javac \
+  --disable-jit"
+
+asdf install erlang 24.1.2
