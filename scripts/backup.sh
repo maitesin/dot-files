8680ed04c5525b6363d95f6481be36036f7db7b0
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2022-03-26 18:41:06 +0100

    First approach to the backup script

---
+++ scripts/backup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+USB_PATH="/mnt"
+DADES_PATH="/Dades"
+
+# Move to the working directory
+pushd ${USB_PATH}
+
+# Create a new folder for the backup
+BKP_PATH="backup-${date +%F}"
+mkdir ${BKP_PATH}
+
+# Move to the backup folder
+pushd ${BKP_PATH}
+
+# Backup the Backup folder
+rsync -av --progress --append ${DADES_PATH}/Backup .
