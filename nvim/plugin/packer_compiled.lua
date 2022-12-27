52027ac1c83e2e7ebc20233a83a86d2fea161a12
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2022-12-27 20:01:03 +0100

    Update the packer_compiled to include vimwiki

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -153,6 +153,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/maitesin/.local/share/nvim/site/pack/packer/start/vim-surround",
     url = "https://github.com/tpope/vim-surround"
+  },
+  vimwiki = {
+    loaded = true,
+    path = "/home/maitesin/.local/share/nvim/site/pack/packer/start/vimwiki",
+    url = "https://github.com/vimwiki/vimwiki"
   }
 }
 
