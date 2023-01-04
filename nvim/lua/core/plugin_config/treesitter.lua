5a75359729417d9b3140b9b168cfca9ba3560cb7
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2023-01-04 21:05:50 +0100

    Update treesitter to have the same format as the other plugin configs

--- nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/lua/core/plugin_config/treesitter.lua
@@ -1,8 +1,8 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup ({
   ensure_installed = {'vim', 'lua', 'c', 'rust', 'elixir', 'python'},
   sync_install = false,
   auto_install = true,
   highlight = {
     enable = true,
   },
-}
+})
