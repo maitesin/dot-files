a477107ba3784e1f9ec8be0a1f5cd5a53a035b3e
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2023-05-29 12:10:14 +0200

    Fix the setting of the NAP config

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@
 set -gx GOPATH $HOME/dev/go
 set -gx PATH $PATH $GOPATH/bin $HOME/.cargo/bin
 set -gx EDITOR nvim
-set -gx NAP_CONFIG="~/.local/share/nap"
+set -gx NAP_CONFIG ~/.local/share/nap
 
 alias ls="exa -lgHSI snap --git --time-style long-iso"
 alias cat="bat --theme gruvbox-dark"
