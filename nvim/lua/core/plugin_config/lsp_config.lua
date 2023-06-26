9b7d019aaaa662e6e777ba53dd9cecb96f8f89af
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2023-06-26 15:38:57 +0200

    Update the lsp config

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,6 +1,6 @@
 require('mason').setup()
 require('mason-lspconfig').setup({
-  ensure_installed = {'sumneko_lua', 'elixirls'}
+  ensure_installed = {'lua_ls', 'elixirls'}
 })
 
 local on_attach = function(_, _)
@@ -14,6 +14,6 @@ local on_attach = function(_, _)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
-require('lspconfig').sumneko_lua.setup{}
+require('lspconfig').lua_ls.setup{}
 
 require('lspconfig').elixirls.setup{}
