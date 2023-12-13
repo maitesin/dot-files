3a65524fa847115469b0d24e0d2cbf8d6a564974
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2023-12-13 10:01:18 +0100

    Improve the scripts

--- scripts/mtg/draft.py
+++ scripts/mtg/draft.py
@@ -4,13 +4,13 @@ from itertools import product
 import random
 
 colors = ['red', 'green', 'blue', 'yellow', 'orange']
-numbers = [1, 2, 3, 4, 5, 6, 7]
+numbers = [i for i in range(1, 15)]
 packs = []
 
 for r in product(colors, numbers):
     packs.append(f'{r[0]} {r[1]}')
 
-drafters = ['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5', 'Player 6', 'Player 7', 'Player8']
+drafters = [f'Player {i}' for i in range(1,9)]
 
 for drafter in drafters:
     print(f'{drafter}:')
