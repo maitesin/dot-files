1acb0092a89b19f3db7d26d87337f87a8e8344d8
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2024-01-30 11:52:20 +0100

    Remove LUA support

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,6 +1,6 @@
 require('mason').setup()
 require('mason-lspconfig').setup({
-  ensure_installed = {'lua_ls', 'elixirls'}
+  ensure_installed = {'elixirls'}
 })
 
 local on_attach = function(_, _)
@@ -13,7 +13,5 @@ local on_attach = function(_, _)
   vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_reference, {})
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
-
-require('lspconfig').lua_ls.setup{}
 
 require('lspconfig').elixirls.setup{}
--- nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/lua/core/plugin_config/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup ({
-  ensure_installed = {'vim', 'lua', 'c', 'rust', 'elixir', 'python'},
+  ensure_installed = {'vim', 'c', 'rust', 'elixir', 'python'},
   sync_install = false,
   auto_install = true,
   highlight = {
