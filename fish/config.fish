5fac29e2996944c17c34d53097f06f3e5af65d32
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2024-07-11 15:25:51 +0200

    Add TINYGO env variable

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,8 @@
 set -gx GOPATH $HOME/dev/go
 set -gx PATH $PATH $GOPATH/bin $HOME/.cargo/bin
 set -gx EDITOR nvim
+set -gx TINYGO $HOME/dev/tinygo
+set -gx PATH $PATH $TINYGO/bin
 
 alias ls="eza -lgHSI snap --git --time-style long-iso"
 alias cat="bat --theme gruvbox-dark"
